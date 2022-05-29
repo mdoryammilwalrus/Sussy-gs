7db9c08c10ec3936c532ca6bdc70acd4378731d8
mdoryammilwalrus <91034348+mdoryammilwalrus@users.noreply.github.com>
2022-05-28 19:22:44 -0500

    Update gs.js

--- static/script/gs.js
+++ static/script/gs.js
@@ -68,10 +68,6 @@ async function compileGs(app) {
         featured: [],
         web: [],
         indie: [],
-        nes: [],
-        snes: [],
-        gba: [],
-        sega: [],
         gfn: [],
         n64: [],
     };
@@ -213,20 +209,6 @@ async function compileGs(app) {
         class: 'data-section nintendo category',
         attrs: {
             'data-category': 'nintendo'
-        }
-    }), app.createElement('section', [app.createElement('span', 'Sega Genesis', {
-        style: {
-            display: 'block',
-            'margin-bottom': '30px',
-            'font-size': '18px',
-            'font-weight': '500'
-        }
-    }), app.createElement('div', list.sega, {
-        class: 'gs-library'
-    })], {
-        class: 'data-section sega category',
-        attrs: {
-            'data-category': 'sega'
         }
     }), app.createElement('section', [app.createElement('span', 'GeForce Now', {
         style: {
